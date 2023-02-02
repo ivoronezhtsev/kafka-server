29e148c1a4eb00c5edcf269ed0875527fe9c5081
igor <i.voronezhtsev@gmail.com>
2023-02-02 21:16:33 +0300

    Фикс для сборки и запуска у меня

--- src/main/java/ru/xpendence/kafkaserver/KafkaServerApplication.java
+++ src/main/java/ru/xpendence/kafkaserver/KafkaServerApplication.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
@@ -15,9 +17,9 @@ import java.io.IOException;
 
 @SpringBootApplication
 @EnableKafka
-//@EnableAutoConfiguration(exclude = {
-//        KafkaAutoConfiguration.class
-//})
+@EnableAutoConfiguration(exclude = {
+        KafkaAutoConfiguration.class
+})
 @ComponentScan(basePackages = {
         "ru.xpendence.kafkaserver.config",
         "ru.xpendence.kafkaserver.controller",
